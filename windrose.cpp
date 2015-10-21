aa65ff9c487352c958e92d6fbb51a540bbebcc28
Kaustubh Walokar <kaustubh@Kaustubhs-MacBook-Air.local>
2015-10-20 19:09:10 -0700

    running code updated

--- windrose.cpp
+++ windrose.cpp
@@ -65,15 +65,21 @@ void displayArray()
 			{
 				cout<<wr[i][j]<<"\t";
 			}
-	cout<<endl;
-}
- 
+			cout<<endl;
+	}
+
+	// for(int i =0; i<NUM_SPEEDBINS*NUM_SECTORS;i++)
+	// 	cout<<*wr[i];
+	
+	
+
+
 }
 
 int main(int argc, const char* argv[])
 {
 
-#pragma omp parallel for
+//#pragma omp parallel for
 	for(int j=1;j<argc;j++)
  	{
  		
